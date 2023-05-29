3400b83372a1151ec8eb14eafe2a1376a64a1659
Katharine Lin <katharine.exe@gmail.com>
2023-05-29 18:02:08 +0200

    added booking model validations

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,8 +2,9 @@ class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :dog
 
-  validates :user_id, presence: true
+  validates :user_id, presence: true, uniqueness: { scope: :dog_id }
+  validates :dog_id, presence: true
   validates :status, presence: true, inclusion: { in: %w[pending accepted denied] }
-  validates :photo_url, presence: true
-  validates :name, presence: true
+  validates :start_date, presence: true
+  validates :end_date, presence: true
 end
