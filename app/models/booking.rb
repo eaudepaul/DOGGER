b05d2e0a49d7d607af76a8ca524f8ab2ca534ed4
Katharine Lin <katharine.exe@gmail.com>
2023-05-29 17:48:23 +0200

    added dog model validations

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,9 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :dog
+
+  validates :user_id, presence: true
+  validates :status, presence: true, inclusion: { in: %w[pending accepted denied] }
+  validates :photo_url, presence: true
+  validates :name, presence: true
 end
