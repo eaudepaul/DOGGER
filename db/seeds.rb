ea90c9d949c5fc9ee95370897c8c5203c06318f4
Charlotte de Janzé <130290009+dejanze@users.noreply.github.com>
2023-05-30 11:46:54 +0200

    seeding dogs

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+puts "deleting dogs..."
+
+Dog.destroy_all
+
+puts "creating dogs..."
+
+Dog.create(name: "Heidi", breed: "Dackel", age: 7, photo_url: "https://res.cloudinary.com/di0qqolg7/image/upload/v1685438162/IMG_0955_lpuxfg.jpg")
+Dog.create(name: "Henry", breed: "Cute Dog", age: 2, photo_url: "https://res.cloudinary.com/di0qqolg7/image/upload/v1685438162/IMG_0955_lpuxfg.jpg")
+Dog.create(name: "Paul", breed: "Irish Setter", age: 1, photo_url: "https://res.cloudinary.com/di0qqolg7/image/upload/v1685438134/IMG_0536_kbzb32.jpg")
+Dog.create(name: "Baran", breed: "Fluffball", age: 3, photo_url: "https://res.cloudinary.com/di0qqolg7/image/upload/v1685439839/image1_ins4de.png")
+Dog.create(name: "Toni", breed: "Chihuaha", age: 5, photo_url: "https://res.cloudinary.com/di0qqolg7/image/upload/v1685438143/IMG_1303_clj4kq.jpg")
+Dog.create(name: "Kathy", breed: "FoxHound", age: 8, photo_url: "https://res.cloudinary.com/di0qqolg7/image/upload/v1685438162/IMG_0955_lpuxfg.jpg")
+
+puts "done!"
