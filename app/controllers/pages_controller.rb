9c9571da938aea41d620c7a7b7f5cfe0cdf3a08f
antonio celano <antonio_celano@hotmail.com>
2023-05-31 15:25:28 +0200

    Add dogs carousel to home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :index
 
   def index
+    @dog = Dog.all.sample
+    @dogs = Dog.where.not(id: @dog.id).sample(5)
   end
 end
