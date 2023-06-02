92e468cc2db90922adb6a97ac9719dedbbb1ab83
Katharine Lin <katharine.exe@gmail.com>
2023-06-02 11:10:59 +0200

    added cuddliness, address, details validations

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -6,7 +6,7 @@ class Dog < ApplicationRecord
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
 
-  validates :name, :breed, :age, presence: true
+  validates :name, :breed, :age, :details, :cuddliness, :address, presence: true
   validates :age, numericality: { only_integer: true }
   validates :age, numericality: { greater_than: 0 }
 end
